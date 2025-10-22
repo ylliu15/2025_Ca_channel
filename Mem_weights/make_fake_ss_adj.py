9cc39fff58ce5ab9d2e7c52ae935a6288c659f74
Yulai Liu <ylliu970614@gmail.com>
2025-10-21 18:09:29 -0700

    Update make_fake_ss_adj.py

--- Mem_weights/make_fake_ss_adj.py
+++ Mem_weights/make_fake_ss_adj.py
@@ -1,8 +1,8 @@
 #!/software/conda/envs/PPI_design/bin/python
 import os,sys,glob,torch
 
-# here 888 is the length of the full protein complex - change accordingly
-ss=torch.full((888,), 3).float() # 3 is mask
-adj=torch.full((888,888), 2).float() # 2 is mask
+# here 932 is the length of the full protein complex - change accordingly
+ss=torch.full((932,), 3).float() # 3 is mask
+adj=torch.full((932,932), 2).float() # 2 is mask
 torch.save(ss, '/path/to/fake_scaffold/fake_ss.pt')
 torch.save(adj, '/path/to/fake_scaffold/fake_adj.pt')
